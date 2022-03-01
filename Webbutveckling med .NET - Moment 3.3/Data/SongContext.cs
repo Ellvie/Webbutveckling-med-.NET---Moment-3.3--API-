5296b399d0a2077dcdac8b0a706f73cbdc8b6c50
Ellvie <90103271+Ellvie@users.noreply.github.com>
2022-03-01 17:45:34 +0100

    Initial commit

---
+++ Webbutveckling med .NET - Moment 3.3/Data/SongContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Webbutveckling_med_.NET___Moment_3._3.Models;
+
+namespace Webbutveckling_med_.NET___Moment_3._3.Data
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options) : base(options)
+        {
+
+        }
+
+
+        public DbSet<Song> Songs { get; set; }
+    }
+}
---
+++ Webbutveckling med .NET - Moment 3.3/Models/Song.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Webbutveckling_med_.NET___Moment_3._3.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string? Artist { get; set; }
+
+        [Required]
+        public string? Title { get; set; }
+
+        [Required]
+        public int Length { get; set; }
+
+        [Required]
+        public string? Category { get; set; }
+    }
+}
